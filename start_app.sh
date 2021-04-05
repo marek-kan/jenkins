b498d45aaaa4046e4a0d4829f179c8b7d48fbad0
Marek Kancir <kancir.marek@gmail.com>
2021-04-05 11:52:39 +0200

    set up starting from Jenkins

--- start_app.sh
+++ start_app.sh
@@ -1,1 +1,1 @@
-docker exec -it flask_app gunicorn -b 0.0.0.0:8080 usr.test_deploy.wsgi:application
+docker exec -d flask_app gunicorn -b 0.0.0.0:8080 usr.test_deploy.wsgi:application
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
 # in docker call gunicorn -b 0.0.0.0:9999 wsgi:application
+import os
+import sys
+
+try:
+    sys.path.append(r'usr/test_deploy/')
+except:
+    print('Failed to add path!')
+
 from app import get_app
 
 application = get_app()
